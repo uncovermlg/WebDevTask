33d27ffdaf3662b818e7402b5e75fd278d68245d
uncovermlg <63254337+uncovermlg@users.noreply.github.com>
2020-04-26 20:31:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+//Predefined scores
+localStorage.setItem("h1",200000);
+localStorage.setItem("h2",200000);
+localStorage.setItem("h3",200000);
+localStorage.setItem("h4",200000);
+localStorage.setItem("h5",200000);
+// Variables to hold the current high score values. These are changed after the game
+let s1=localStorage.getItem("h1");
+let s2=localStorage.getItem("h2");
+let s3=localStorage.getItem("h3");
+let s4=localStorage.getItem("h4");
+let s5=localStorage.getItem("h5");
+
+
 let milli=0;
 let sec=0;  //Define variables to hold time values
 let min=0;
@@ -12,18 +26,32 @@ let smallest=0; //Variable to hold the index of the cell with the smallest numbe
 let a=[];
 for (let i=0;i<20;i++) a[i]=i+1;
 
+//variable to hold setInterval() function and stopwatch status
+let interval=null;
+let status="stop";
+
 //Variable to hold the values in the grid
 let grid=document.getElementsByClassName("cell");
+ for (let j=0;j<20;j++){
+
+       grid[j].addEventListener("click",function (){
+        if(smallest==j) game(j);
+        });
+    }
+
+//Function to start a new game:
+function newgame(){
+    //alert(document.getElementById("1").innerHTML);
+    initialise();
+    status="stop";
+    window.clearInterval(interval);
+    milli=0;
+    min=0;
+    sec=0;
+    document.getElementById("display").innerHTML= "00:00:00";
 
 
-//Function to check is all numbers have been clicked
-/*function checknull()
-{
-for (let i=0;i<20;i++){
-if (grid[i].innerHTML != null) return 0;
-}
-return 1;
-}*/
+    }
 
 //Function to find the index if the smallest number
 function smallnum(x){
@@ -53,46 +81,113 @@ if (grid[i].innerHTML==1) smallest=i;}
 }
 
 //Function executed while the user is clicking
-function clickmode(){
+/*function clickmode(){
+
  for (let j=0;j<20;j++){
-       // checknull();
-        grid[j].addEventListener("click",function (){
+
+       grid[j].addEventListener("click",function (){
         if(smallest==j) game(j);
         });
     }
+}*/
+
+//Funtion to covert scores to corresponding timings
+function time(x){
+if (x==200000) return 00+":"+00+":"+00;
+else{
+let dm,ds,dmi="0";
+let m= Math.floor(x/60000);
+if(m<10) dm="0"+m;
+let s= Math.floor((x-m*60000)/1000);
+if(s<10) ds="0"+s;
+let mi= x-m*60000-s*1000;
+if(mi<10) dmi="0"+mi;
+return dm+ ":" + d + ":" + dmi;
+}
 }
 
+//Function to save score and update the table
+function savegame(n,score){
+
+    if (n==1){
+     [s5,s4]=[s4,s5];
+     [s4,s3]=[s3,s4];
+     [s3,s2]=[s2,s3];
+     [s2,s1]=[s1,s2];
+     [s1,score]=[score,s1];
+    }
+    else if(n==2) {
+    [s5,s4]=[s4,s5];
+    [s4,s3]=[s3,s4];
+    [s3,s2]=[s2,s3];
+    [s2,score]=[score,s2];
+    }
+    else if(n==3){
+    [s5,s4]=[s4,s5];
+    [s4,s3]=[s3,s4];
+    [s3,score]=[score,s3];
+    }
+    else if(n==4){
+    [s5,s4]=[s4,s5];
+    [s4,score]=[score,s4];
+
+    }
+    else if(n==5){
+    [s5,score]=[score,s5];
+    }
+
+
+    document.getElementById("H1").innerHTML= time(s1);
+    document.getElementById("H2").innerHTML= time(s2);
+    document.getElementById("H3").innerHTML= time(s3);
+    document.getElementById("H4").innerHTML= time(s4);
+    document.getElementById("H5").innerHTML= time(s5);
+}
 
 //Function for game mechanics
 function game(index){
-   if(game[index].innerHTML==1) startstop();
-   if(game[index].innerHTML==40) {
-   startstop();
-   grid[index].innerHTML=null;
-   alert("Your timing is" + dmin + ":" + dsec + ":" + dmilli);
-   //savegame(); (to be added later)
-   newgame();
-    }
-    if(grid[smallest].innerHTML>= 21){
-        grid[smallest].innerHTML=null;
-        }
-    else
-      { grid[smallest].innerHTML+=20;
+   if(grid[index].innerHTML == 1) {startstop();}
 
-      smallest=smallnum(grid[smallest].innerHTML -20 +1); //passes the next smallest number to search for
-      }
-      click();
+// This if condition is a set of all things to be executed once the game is over
+   if(grid[index].innerHTML==40) {
+
+   grid[index].innerHTML=null;
+   startstop();
+   alert("Your timing is" + dmin + ":" + dsec + ":" + dmilli);
+   let score= parseInt(dmin)*60000 + parseInt(dsec)*1000 + parseInt(dmilli);
+
+
+    for(let i=0; i<localStorage.length; i++) {
+    //alert(score);
+        let key = localStorage.key(i);
+        alert(`${key}: ${localStorage.getItem(key)}`);
+        if(key=="h1" && localStorage.getItem(key)>score){ savegame(1,score);}
+        else if(key=="h2" && localStorage.getItem(key)>score){ savegame(2,score);}
+        else if(key=="h3" && localStorage.getItem(key)>score){ savegame(3,score);}
+        else if(key=="h4" && localStorage.getItem(key)>score){ savegame(4,score);}
+        else if(key=="h5" && localStorage.getItem(key)>score){ savegame(5,score);}
+        else alert("You did not beat the high scores :( ");
+
+        //alert(`${key}: ${localStorage.getItem(key)}`);
+    }
+   newgame();
+   }
+
+    smallest=smallnum(parseInt(grid[index].innerHTML) +1);
+    if(grid[index].innerHTML >= 21){
+        grid[index].innerHTML=null;
+
+     }
+    else
+      { grid[index].innerHTML=20+parseInt(grid[index].innerHTML);}
+
+       //passes the next smallest number to search for
+
+
 }
 
-
-
-//variable to hold setInterval() function and stopwatch status
-let interval=null;
-let status="stop";
-
 //Stopwatch function:
-function stopwatch()
-{
+function stopwatch(){
     milli++;
     if (milli/100==1)
     {   milli=0;
@@ -115,31 +210,20 @@ document.getElementById("display").innerHTML= dmin + ":" + dsec + ":" + dmilli;
 }
 
 //Function to control stop watch.
-function startstop()
-{
+function startstop(){
 if(status=="stop")
 {
 interval=window.setInterval(stopwatch,10);
-//document.getElementById("startstop").innerHTML="Stop";
+
 status="start";
 }
 else{
 window.clearInterval(interval);
-//document.getElementById("startstop").innerHTML="Start";
 status="stop";
 }
 }
 
-//Function to reset the stopwatch:
-function newgame(){
-    window.clearInterval(interval);
-    milli=0;
-    min=0;
-    sec=0;
-    document.getElementById("display").innerHTML= "00:00:00";
-    initialise();
-   clickmode();
-    }
+
 
 
 
